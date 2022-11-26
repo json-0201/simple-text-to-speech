b551700d6b7e2b1e74d2f78d72f8313b1f4ab371
json-0201 <106159207+json-0201@users.noreply.github.com>
2022-11-25 22:00:33 -0800

    Update tts.py

--- tts.py
+++ tts.py
@@ -7,13 +7,13 @@ from playsound import playsound
 file_name = "sample.mp3"
 
 # language: English
-# text = "Imagine that you have just arrived at a hotel after a tiring 7-hour overnight flight."
+# text = "Not All Of Us Can Afford To Be Romantic."
 # tts_en = gTTS(text=text, lang="en")
 # tts_en.save(file_name)
 # playsound(file_name)
 
 # language: Korean
-# text = "라이브러리를 활용한 데이터 과학과 머신러닝"
+# text = "편견은 내가 다른 사람을 사랑하지 못하게 하고, 오만은 다른 사람이 나를 사랑하지 못하게 한다."
 # tts_ko = gTTS(text=text, lang="ko")
 # tts_ko.save(file_name)
 # playsound(file_name)
